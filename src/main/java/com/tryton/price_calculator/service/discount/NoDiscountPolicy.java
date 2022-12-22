b5a2fdff18da5f128f48f69e49ef6b2f60dd8875
mephisto <mephisto2120@gmail.com>
2022-12-22 15:45:21 +0100

    Fixing no discount policy

--- src/main/java/com/tryton/price_calculator/service/discount/NoDiscountPolicy.java
+++ src/main/java/com/tryton/price_calculator/service/discount/NoDiscountPolicy.java
@@ -3,11 +3,14 @@ package com.tryton.price_calculator.service.discount;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Component
 public class NoDiscountPolicy implements DiscountPolicy {
     @Override
     public BigDecimal applyDiscount(CalculationContext calculationContext) {
-        return calculationContext.getPrice();
+        BigDecimal price = calculationContext.getPrice();
+        return price.multiply(BigDecimal.valueOf(calculationContext.getQuantity()))
+                .setScale(2, RoundingMode.CEILING);
     }
 }
--- src/test/java/com/tryton/price_calculator/service/discount/NoDiscountPolicyTest.java
+++ src/test/java/com/tryton/price_calculator/service/discount/NoDiscountPolicyTest.java
@@ -1,30 +1,43 @@
 package com.tryton.price_calculator.service.discount;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class NoDiscountPolicyTest {
     private static final BigDecimal INITIAL_PRICE = BigDecimal.TEN;
-    private static final int QUANTITY = 1000_000;
+    private static final int QUANTITY_1 = 5;
+    private static final int QUANTITY_2 = 20;
+    private static final int DEFAULT_SCALE = 2;
 
     private final NoDiscountPolicy noDiscountPolicy = new NoDiscountPolicy();
 
-    @Test
-    void shouldApplyNoDiscount() {
+    @ParameterizedTest
+    @MethodSource
+    void shouldApplyNoDiscount(int quantity, BigDecimal expected) {
         //given
         CalculationContext calculationContext = DefaultCalculationContext.builder()
                 .price(INITIAL_PRICE)
-                .quantity(QUANTITY)
+                .quantity(quantity)
                 .build();
 
         //when
         BigDecimal price = noDiscountPolicy.applyDiscount(calculationContext);
 
         //then
-        assertThat(price).isEqualTo(INITIAL_PRICE);
+        assertThat(price).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> shouldApplyNoDiscount() {
+        return Stream.of(
+                Arguments.of(QUANTITY_1, BigDecimal.valueOf(50).setScale(DEFAULT_SCALE)),
+                Arguments.of(QUANTITY_2, BigDecimal.valueOf(200).setScale(DEFAULT_SCALE)),
+                Arguments.of(0, BigDecimal.ZERO.setScale(DEFAULT_SCALE))
+        );
     }
 }
